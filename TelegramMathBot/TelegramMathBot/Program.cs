fde490b0209daae6d94e4d3c19f6916bd31d1f3f
fregire <fregire@mail.ru>
2020-10-10 18:53:52 +0500

    InitChanges

---
+++ TelegramMathBot/TelegramMathBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramMathBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
