8f0354312ea7423c2e791737c8a3b16adba5c79c
= <=>
2020-11-08 15:22:39 +0500

    Added base functionality

---
+++ TelegramMathBot/TelegramMathBot/MathBot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramMathBot
+{
+    public class MathBot
+    {
+        private readonly string token;
+        private readonly TelegramBotClient bot;
+        public MathBot(string token)
+        {
+            this.token = token;
+            this.bot = new TelegramBotClient(token);
+            this.bot.OnMessage += OnMessageReceived;
+        }
+
+        public void Start()
+        {   
+            bot.StartReceiving();
+            while (true);
+        }
+
+        private async void OnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            Console.WriteLine(messageEventArgs.Message.Text);
+        }
+    }
+}
--- TelegramMathBot/TelegramMathBot/Program.cs
+++ TelegramMathBot/TelegramMathBot/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TelegramMathBot
 {
     class Program
     {
+        static readonly string token = "1495097120:AAHpmNmtzpgF6-_BZe0yXyGdfQYrUdhMokQ";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bot = new MathBot(token);
+            bot.Start();
         }
     }
 }
