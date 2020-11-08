3dd8bfd3d980de62fcb2db25adcceae891dcc84a
= <=>
2020-11-08 15:29:27 +0500

    vfvf

--- TelegramMathBot/TelegramMathBot/Program.cs
+++ TelegramMathBot/TelegramMathBot/Program.cs
@@ -6,7 +6,7 @@ namespace TelegramMathBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
